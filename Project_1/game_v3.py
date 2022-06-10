a60ca771cb83cfec2a0882bfd98ae08497edab54
shubinilya <shubini@mail.ru>
2022-06-10 09:37:52 +0300

    Версия game3

--- Project_1/game_v3.py
+++ Project_1/game_v3.py
@@ -1,12 +1,12 @@
 """Игра угадай число
-Компьютер сам загадывает и сам угадывает число
+Компьютер сам загадывает и сам угадывает число методом последовательного приближения
 """
 
 import numpy as np
 
 
 def approx_predict(number: int = 1) -> int:
-    """Методом приближения угадываем число
+    """Методом последовательного приближения угадываем число
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -20,10 +20,13 @@ def approx_predict(number: int = 1) -> int:
     
     while True:
         count += 1
-        predict_number = np.random.randint(min_number, max_number+1)  # предполагаемое число
+        
+        predict_number = np.random.randint(min_number, max_number+1)  # произвольное предполагаемое число в диапазоне
+        
         if number == predict_number:
             break  # выход из цикла если угадали
         else:
+            # меняем диапазон поиска
             if predict_number < number:
                 min_number = predict_number
             else:
@@ -31,27 +34,29 @@ def approx_predict(number: int = 1) -> int:
     return count
 
 
-def score_game(random_predict) -> int:
+def score_game(approx_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        approx_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
     count_ls = []
+
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(approx_predict(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print(count_ls)
     return score
 
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(approx_predict)
